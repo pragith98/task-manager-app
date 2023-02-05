ca60199c8e7e9327c34a79818500eec8b141096a
pragith98 <lakshanxp@hotmail.com>
2023-02-06 01:26:44 +0530

    create task form

--- src/pages/AddNew/AddNew.jsx
+++ src/pages/AddNew/AddNew.jsx
@@ -1,9 +1,45 @@
-function AddNew(){
-    return(
-        <div className="container">
-            <h1>Add new task</h1>
+function AddNew() {
+  return (
+    <div className="container">
+      
+      <div className="row justify-content-center mt-2">
+        <div className="col-6">
+        <legend>Add new task</legend>
+          <form>
+            <fieldset>
+              <div className="form-group">
+                <label>Task Name</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter task name"
+                />
+              </div>
+
+              <br />
+
+              <div className="form-group">
+                <label>Task</label>
+                <textarea
+                  className="form-control"
+                  id="exampleTextarea"
+                  rows="3"
+                ></textarea>
+              </div>
+
+              <br />
+
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </fieldset>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default AddNew;
