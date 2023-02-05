4decaa5334e7cfd9c9a397348f96d31fba0d41e4
pragith98 <lakshanxp@hotmail.com>
2023-02-06 03:12:12 +0530

    rename task list component name

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header/Header";
-import Tasks from "./pages/Tasks/Tasks";
+import Tasks from "./pages/Tasks/TaskList";
 import AddNew from "./pages/AddNew/AddNew";
 
 function App() {
