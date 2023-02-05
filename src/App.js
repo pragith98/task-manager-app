d30d9ef8053a14d61e604a0ec8fed76385852b37
pragith98 <lakshanxp@hotmail.com>
2023-02-06 01:10:00 +0530

    add routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header/Header";
+import Tasks from "./pages/Tasks/Tasks";
+import AddNew from "./pages/AddNew/AddNew";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/addnew" element={<AddNew/>} />
+          <Route path="/" element={<Tasks/>} />
+        </Routes>
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
 
